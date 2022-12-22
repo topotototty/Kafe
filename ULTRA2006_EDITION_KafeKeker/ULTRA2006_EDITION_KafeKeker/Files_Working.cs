dd5c2ba91173c95c2b77cc37610ff077f58a73fb
topotototty <112858195+topotototty@users.noreply.github.com>
2022-12-22 10:49:10 +0300

    Add files via upload

---
+++ ULTRA2006_EDITION_KafeKeker/ULTRA2006_EDITION_KafeKeker/Files_Working.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ULTRA2006_EDITION_KafeKeker
+{
+    static class Files_Working
+    {
+        static public void SaveToFile<T>(T list, string path)
+        {
+            if (!File.Exists(Environment.SystemDirectory + "/" + path))
+            { 
+                FileStream fileStream = File.Create(Environment.SystemDirectory + "/" + path);
+                fileStream.Dispose();
+            }
+
+            string json = JsonConvert.SerializeObject(list);
+            File.WriteAllText(Environment.SystemDirectory + "/" + path, json);
+        }
+
+        static public List<User> ReadUsersFromFile()
+        {
+
+            List<User> users;
+            if (!File.Exists("Users.json"))
+            {
+                FileStream fileStream = File.Create("Users.json");
+                fileStream.Dispose();
+
+                users = new List<User>();
+                users.Add(new User(0, 0, "Admin", "Admin"));
+                SaveToFile(users, "Users.json");
+            }
+            else
+            {
+                string usersInfo = File.ReadAllText("Users.json");
+                users = JsonConvert.DeserializeObject<List<User>>(usersInfo);
+            }
+            return users;
+        }
+
+        static public List<Transaction> ReadFromFile(string path)
+        {
+            path = "transactions.json";
+            List<Transaction> transactions;
+
+            if (!File.Exists(Environment.SystemDirectory + "/" + path))
+            {
+                FileStream fileStream = File.Create(Environment.SystemDirectory + "/" + path);
+                fileStream.Dispose();
+
+                transactions = new List<Transaction>();
+                transactions.Add(new Transaction(0, "0", 0, DateTime.Now, "+"));
+                Files_Working.SaveToFile(transactions, "transactions.json");
+            }
+
+            string resultInfo = File.ReadAllText(Environment.SystemDirectory + "/" + path);
+            transactions = JsonConvert.DeserializeObject<List<Transaction>>(resultInfo);
+
+            return transactions;
+        }
+    }
+}
---
+++ ULTRA2006_EDITION_KafeKeker/ULTRA2006_EDITION_KafeKeker/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ULTRA2006_EDITION_KafeKeker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Accountant accountant = new Accountant();
+             accountant.Accountant_Menu();*/
+            Main_Menu menu = new Main_Menu();
+            menu.Menu();
+            /*Admin menu = new Admin();
+            menu.Admin_Menu();*/
+        }
+    }
+}
